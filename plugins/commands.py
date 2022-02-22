de9a510eedbea01be3d8305f5bb91c803bb125cb
Unreasonable motto <90086313+Athul1223@users.noreply.github.com>
2022-02-22 18:04:41 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -115,11 +115,12 @@ async def help(client, message):
 
 @Client.on_message(filters.command('status') & filters.private)
 async def status(client, message):
-    await asyncio.sleep(3)
+    await asyncio.sleep(4)
     sm=await message.reply_text(
            text=script.STATUS_TXT.format(message.from_user.mention),
            reply_to_message_id=message.message_id,
     )
+    await asyncio.sleep(3)
     await sm.delete()
     await message.delete()
 
